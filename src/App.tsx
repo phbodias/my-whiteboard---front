ce0942fb2565ae3a99faaa609d1da6811244db3b
phbodias <pedrohenriquebritooliveira@gmail.com>
2022-11-10 21:11:48 -0300

    :lipstick: Global Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home";
+import GlobalStyle from "./styles/globalStyle";
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
       </Routes>
