afb10ee2c36f96baa72d549ba2cd2033be00bc24
phbodias <pedrohenriquebritooliveira@gmail.com>
2022-11-10 21:37:03 -0300

    :heavy_plus_sign: build: Adicionadas dependencias e tipos do react icons

---
+++ src/components/Header/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Content = styled.div`
+  height: 60px;
+  border-bottom: solid 1px #dddddd;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 25px;
+  box-sizing: border-box;
+  color: #837f7b;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition-timing-function: linear;
+  transition-duration: 0.5s;
+`;
+
+export default Content;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
+import Header from "../../components/Header";
+
 const HomePage = () => {
-  return <>Home</>;
+  return <Header />;
 };
 
 export default HomePage;
