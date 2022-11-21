3a61f91a0857168b15cef1f9c9af76815f7f197f
Pedro Dias <pedrohenriquebritooliveira@gmail.com>
2022-11-21 19:55:48 -0300

    :sparkles: Serviço para logar usuário

---
+++ src/services/signin.ts
@@ -0,0 +1,7 @@
+import api from "./api";
+import authInterface from "../interfaces/authInterface";
+
+export async function signIn(user: authInterface) {
+  const response = await api.post("/sign-in", { user });
+  return response.data;
+}
